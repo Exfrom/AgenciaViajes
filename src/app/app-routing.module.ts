51d34bf61fe71903bf8175086b9a5b099f62d3e8
Diana Shirley Celis Bautista <110642260+Exfrom@users.noreply.github.com>
2023-09-11 21:19:12 -0500

    secreaproyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  { path: 'registro', component: RegistroComponent },
+  { path: 'reserva', component: ReservaComponent },
+  { path: 'principal', component: PrincipalComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    ReservaComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
